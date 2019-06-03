6b3b12b43518c83b2a2e0a9247d20e6e1f674829
Twan Natter <twan180401@gmail.com>
2019-06-03 11:52:37 +0200

    Add project files.

---
+++ Sprint 13 mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sprint_13_mvc.Startup))]
+namespace Sprint_13_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
